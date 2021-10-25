a2d2e56c0b6be4f32d95ae1bb6456c338283f3c9
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2021-10-24 23:52:19 -0400

    making server post friendly

--- __init__.py
+++ __init__.py
@@ -9,18 +9,18 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/hackathons/', methods=['GET'])
+@app.route('/hackathons/', methods=['GET','POST'])
 def hackathons():
     options = request.get_json()
     return {"hackathons": get_hackathons(amount=int(request.args.get('amount')), options=options)}
 
 
-@app.route('/projects/', methods=['GET'])
+@app.route('/projects/', methods=['GET','POST'])
 def projects():
     return get_projects(amount=int(request.args.get('amount')))
 
 
-@app.route('/hackathons/projects/',methods=['GET'])
+@app.route('/hackathons/projects/',methods=['GET','POST'])
 def hackathon_projects():
     data = request.get_json()
     category = data['category'] if 'category' in data else None
@@ -28,13 +28,13 @@ def hackathon_projects():
     return {"projects": get_hackathon_projects(data['hackathonUrl'], category, sort_by)}
 
 
-@app.route('/hackathons/categories/',methods=['GET'])
+@app.route('/hackathons/categories/',methods=['GET','POST'])
 def hackathon_categories():
     data = request.get_json()
     return {'categories':[i['name'] for i in get_hackathon_categories(data['hackathonUrl'])]}
 
 
-@app.route('/profile/projects/',methods=['GET'])
+@app.route('/profile/projects/',methods=['GET','POST'])
 def profile_projects():
     data = request.get_json()
     return {'projects':get_profile_projects(data['username'])}
